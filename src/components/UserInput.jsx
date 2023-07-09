38bca4740ba271183dff99814cb2a75dd03c9510
Nuhaj Aziz <nuhaj243@gmail.com>
2023-07-09 21:43:44 +0100

    input border raduis fix

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -7,7 +7,7 @@ export default function UserInput({inputValue, setInputValue, handleSubmit}) {
         <h1 className='text-white text-2xl sm:text-3xl lg:text-4xl font-normal'>IP Address Tracker</h1>
         <div className='flex w-full justify-center'>
             <input type="text" placeholder="Search for any IP address" 
-            className="font-Rubik indent-4 py-3 rounded-l-xl md:w-[40%] lg:w-[35%] xl:w-[25%] outline-none text-lg 
+            className="font-Rubik indent-4 py-3 rounded-l-xl rounded-r-none md:w-[40%] lg:w-[35%] xl:w-[25%] outline-none text-lg 
             placeholder:text-sm sm:placeholder:text-base" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
             <button type='submit' className='bg-black rounded-r-xl px-3 xs:px-6 sm:px-8' onClick={handleSubmit}>
                 <img src={arrow} className='object-contian'/>
